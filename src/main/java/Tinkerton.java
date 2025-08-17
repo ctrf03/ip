4fe43358f67d2f2bc0706d9564b0b1553530ce07
ctrf03 <clintontayrf.ct@gmail.com>
2025-08-18 00:49:19 +0800

    Add support for echo and exit

--- src/main/java/Tinkerton.java
+++ src/main/java/Tinkerton.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
 public class Tinkerton {
+
     public static void intro() {
         System.out.println("Hello! I'm Tinkerton");
-        System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("What can I do for you?");
     }
 
     public static void main(String[] args) {
         intro();
+        Scanner sc = new Scanner(System.in);
+        boolean loop = true;
+
+        while (loop) {
+            String echo = sc.nextLine();
+
+            if (echo.equals("bye")) {
+                loop = false;
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(echo);
+        }
+        sc.close();
+        return;
     }
 }
