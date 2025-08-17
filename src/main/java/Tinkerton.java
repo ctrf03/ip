318a21f25ee964cba1a0f005a44729a8083f9c79
ctrf03 <clintontayrf.ct@gmail.com>
2025-08-18 00:31:42 +0800

    Set intro as a method

--- src/main/java/Tinkerton.java
+++ src/main/java/Tinkerton.java
@@ -1,7 +1,11 @@
 public class Tinkerton {
-    public static void main(String[] args) {
+    public static void intro() {
         System.out.println("Hello! I'm Tinkerton");
         System.out.println("What can I do for you?\n");
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void main(String[] args) {
+        intro();
     }
 }
