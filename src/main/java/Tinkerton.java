08f60e9792fc9924445b97c11cb778e0150f1b60
ctrf03 <clintontayrf.ct@gmail.com>
2025-08-18 13:30:45 +0800

    Add support for add and list

--- src/main/java/Tinkerton.java
+++ src/main/java/Tinkerton.java
@@ -10,17 +10,28 @@ public class Tinkerton {
     public static void main(String[] args) {
         intro();
         Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int count = 0;
         boolean loop = true;
 
         while (loop) {
-            String echo = sc.nextLine();
+            String input = sc.nextLine();
 
-            if (echo.equals("bye")) {
+            if (input.equals("bye")) {
                 loop = false;
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                continue;
+            } else {
+                tasks[count] = input;
+                count++;
+                System.out.println("added " + input);
+                continue;
             }
-            System.out.println(echo);
         }
         sc.close();
         return;
