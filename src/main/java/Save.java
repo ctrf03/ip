9900fef028d183f18d9bdf6e8ae5b220000c78b3
ctrf03 <clintontayrf.ct@gmail.com>
2025-08-23 17:53:29 +0800

    Fix file reading bug

--- src/main/java/Save.java
+++ src/main/java/Save.java
@@ -27,21 +27,21 @@ public class Save {
             while (scanner.hasNext()) {
                 String nextLine = scanner.nextLine();
                 String[] parts = nextLine.split("\\|");
-                String type = parts[0];
-                boolean isCompleted = parts[1].equals("1");
+                String type = parts[0].trim();
+                boolean isCompleted = parts[1].trim().equals("1");
 
                 switch (type) {
                     case "T":
-                        tasks.add(new ToDo(parts[2], isCompleted));
+                        tasks.add(new ToDo(parts[2].trim(), isCompleted));
                         break;
 
                     case "D":
-                        tasks.add(new Deadline(parts[2], isCompleted, parts[3]));
+                        tasks.add(new Deadline(parts[2].trim(), isCompleted, parts[3].trim()));
                         break;
 
                     case "E":
                         String[] range = parts[3].split(" ");
-                        tasks.add(new Event(parts[2], isCompleted, range[0], range[1]));
+                        tasks.add(new Event(parts[2].trim(), isCompleted, range[0], range[1]));
                         break;
 
                     default:
