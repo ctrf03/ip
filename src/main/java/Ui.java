b047aa0efa00815fa987bcb4ccfa00704d100755
ctrf03 <clintontayrf.ct@gmail.com>
2025-08-25 02:02:33 +0800

    Update Ui.java

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Ui {
+
     public void intro() {
         System.out.println("Hello! I'm Tinkerton");
         System.out.println("What can I do for you?");
+    }
+
+    public String readCommand() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        sc.close();
+        return input;
     }
 }
