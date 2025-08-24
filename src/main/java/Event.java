e54fbec02c03d07e12d92c2ec35c0b75af237128
ctrf03 <clintontayrf.ct@gmail.com>
2025-08-24 16:36:29 +0800

    Update Deadline.java with Date class

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,21 +1,25 @@
 public class Event extends Task {
     private String start;
     private String end;
+    private Date startDate;
+    private Date endDate;
 
     public Event(String name, boolean isCompleted, String start, String end) {
         super(name, isCompleted);
         this.start = start;
         this.end = end;
+        this.startDate = new Date(start);
+        this.endDate = new Date(end);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + String.format(" (from: %s to: %s)", start, end);
+        return "[E]" + super.toString() + String.format(" (from: %s to: %s)", this.startDate, this.endDate);
     }
 
     @Override
     public String toFile() {
         String completed = this.isCompleted() ? "1" : "0";
-        return "E | " + completed + " | " + this.name() + " | " + start + " " + end;
+        return "E | " + completed + " | " + this.name() + " | " + this.start + " | " + this.end;
     }
 }
