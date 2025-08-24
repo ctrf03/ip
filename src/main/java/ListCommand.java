ab4c03bc4c514eb43ce6bc4539cc8f7ccc498812
ctrf03 <clintontayrf.ct@gmail.com>
2025-08-25 03:28:20 +0800

    Extract ListCommand.java

---
+++ src/main/java/ListCommand.java
@@ -0,0 +1,24 @@
+public class ListCommand extends Command {
+    public ListCommand(String fullCommand) {
+        super(fullCommand);
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Save save) throws TinkertonException {
+        if (tasks.size() == 0) {
+            throw new TinkertonException(
+                    "I feel like your list is empty so there is no list to show...");
+        }
+
+        ui.print("Here are the tasks in your list:");
+
+        for (int i = 0; i < tasks.size(); i++) {
+            ui.print((i + 1) + ". " + tasks.get(i));
+        }
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+}
