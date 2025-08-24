eac1e03fe01f72ab4d4646ca08bb4e34191a725e
ctrf03 <clintontayrf.ct@gmail.com>
2025-08-24 16:49:53 +0800

    Add check for Event start and end times

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,15 +1,23 @@
+import java.time.LocalDateTime;
+
 public class Event extends Task {
     private String start;
     private String end;
     private Date startDate;
     private Date endDate;
 
-    public Event(String name, boolean isCompleted, String start, String end) {
+    public Event(String name, boolean isCompleted, String start, String end) throws TinkertonException {
         super(name, isCompleted);
         this.start = start;
         this.end = end;
         this.startDate = new Date(start);
         this.endDate = new Date(end);
+
+        if (endDate.date().isBefore(startDate.date())) {
+            throw new TinkertonException("Event that ends before it starts? Doesn't make too much sense right...");
+        } else if (startDate.date().isBefore(LocalDateTime.now())) {
+            throw new TinkertonException("Event that already started in the past? A bit too late to add that...");
+        }
     }
 
     @Override
