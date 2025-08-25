e554ce3d6be066682add2f0f4e36334b42407a73
ctrf03 <clintontayrf.ct@gmail.com>
2025-08-25 15:53:54 +0800

    Move to util package

---
+++ src/main/java/tinkerton/util/CommandType.java
@@ -0,0 +1,11 @@
+public enum CommandType {
+    BYE, LIST, SHOW, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, UNKNOWN;
+
+    public static CommandType command(String input) {
+        try {
+            return CommandType.valueOf(input.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            return UNKNOWN;
+        }
+    }
+}
