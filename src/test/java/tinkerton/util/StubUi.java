872734b9a604a78b3547477954363866ab6ddbe0
ctrf03 <clintontayrf.ct@gmail.com>
2025-08-28 03:04:58 +0800

    Add ToDoCommand.java

--- src/test/java/tinkerton/util/StubUi.java
+++ src/test/java/tinkerton/util/StubUi.java
@@ -1,14 +1,23 @@
 package tinkerton.util;
 
+import java.util.ArrayList;
+
 public class StubUi extends Ui {
-    private String lastPrintedMessage;
+    private ArrayList<String> printedMessages = new ArrayList<>();
 
     @Override
     public void print(String message) {
-        this.lastPrintedMessage = message;
+        printedMessages.add(message);
+    }
+
+    public ArrayList<String> getPrintedMessages() {
+        return printedMessages;
     }
 
     public String getLastPrintedMessage() {
-        return lastPrintedMessage;
+        if (printedMessages.isEmpty()) {
+            return null;
+        }
+        return printedMessages.get(printedMessages.size() - 1);
     }
 }
