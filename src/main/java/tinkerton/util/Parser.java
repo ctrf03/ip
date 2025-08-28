cbebedc533afd9034b0747cc18c55e0dd48ab79d
ctrf03 <clintontayrf.ct@gmail.com>
2025-08-28 20:24:30 +0800

    Add Javadoc for Parser.java

--- src/main/java/tinkerton/util/Parser.java
+++ src/main/java/tinkerton/util/Parser.java
@@ -3,41 +3,51 @@ package tinkerton.util;
 import tinkerton.command.*;
 import tinkerton.core.TinkertonException;
 
+/**
+ * Parses user input commands and returns the corresponding Command object.
+ */
 public class Parser {
+    /**
+     * Parses the full command string and returns the appropriate Command object.
+     *
+     * @param fullCommand The full user input command string.
+     * @return The corresponding Command object.
+     * @throws TinkertonException If the command is unknown or invalid.
+     */
     public static Command parse(String fullCommand) throws TinkertonException {
         String[] parts = fullCommand.split(" ");
         CommandType command = CommandType.command(parts[0]);
 
         switch (command) {
-            case BYE:
-                return new ByeCommand(fullCommand);
+        case BYE:
+            return new ByeCommand(fullCommand);
 
-            case LIST:
-                return new ListCommand(fullCommand);
+        case LIST:
+            return new ListCommand(fullCommand);
 
-            case SHOW:
-                return new ShowCommand(fullCommand);
+        case SHOW:
+            return new ShowCommand(fullCommand);
 
-            case MARK:
-                return new MarkCommand(fullCommand);
+        case MARK:
+            return new MarkCommand(fullCommand);
 
-            case UNMARK:
-                return new UnmarkCommand(fullCommand);
+        case UNMARK:
+            return new UnmarkCommand(fullCommand);
 
-            case TODO:
-                return new ToDoCommand(fullCommand);
+        case TODO:
+            return new ToDoCommand(fullCommand);
 
-            case DEADLINE:
-                return new DeadlineCommand(fullCommand);
+        case DEADLINE:
+            return new DeadlineCommand(fullCommand);
 
-            case EVENT:
-                return new EventCommand(fullCommand);
+        case EVENT:
+            return new EventCommand(fullCommand);
 
-            case DELETE:
-                return new DeleteCommand(fullCommand);
-            case UNKNOWN:
-            default:
-                throw new TinkertonException("Erm... What are you saying?");
+        case DELETE:
+            return new DeleteCommand(fullCommand);
+        case UNKNOWN:
+        default:
+            throw new TinkertonException("Erm... What are you saying?");
         }
     }
 }
