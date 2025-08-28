4204359d748b007d6ac2d48b24eb2594b5cd2b67
ctrf03 <clintontayrf.ct@gmail.com>
2025-08-28 20:29:31 +0800

    Add Javadoc to ShowCommand.java

--- src/main/java/tinkerton/command/ShowCommand.java
+++ src/main/java/tinkerton/command/ShowCommand.java
@@ -6,11 +6,27 @@ import tinkerton.core.TinkertonException;
 import tinkerton.task.TaskList;
 import tinkerton.storage.Save;
 
+/**
+ * Represents a command to show tasks occurring on a specific date.
+ */
 public class ShowCommand extends Command {
+    /**
+     * Constructs a ShowCommand with the full command string.
+     *
+     * @param fullCommand The full user input command string.
+     */
     public ShowCommand(String fullCommand) {
         super(fullCommand);
     }
 
+    /**
+     * Executes the show command, displaying tasks that occur on the specified date.
+     *
+     * @param tasks The list of tasks.
+     * @param ui The user interface handler.
+     * @param save The save handler for persisting tasks.
+     * @throws TinkertonException If the command format is invalid or no tasks are found.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Save save) throws TinkertonException {
         String fullCommand = super.getFull();
@@ -48,6 +64,11 @@ public class ShowCommand extends Command {
         }
     }
 
+    /**
+     * Indicates whether this command should exit the application.
+     *
+     * @return false, as show does not exit the application.
+     */
     @Override
     public boolean isExit() {
         return false;
