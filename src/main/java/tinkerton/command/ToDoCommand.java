05f7492f777b05e45ae0857fec01e99c0a18152b
ctrf03 <clintontayrf.ct@gmail.com>
2025-08-28 20:28:53 +0800

    Add Javadoc to  ToDoCommand.java

--- src/main/java/tinkerton/command/ToDoCommand.java
+++ src/main/java/tinkerton/command/ToDoCommand.java
@@ -6,11 +6,27 @@ import tinkerton.task.TaskList;
 import tinkerton.task.ToDo;
 import tinkerton.storage.Save;
 
+/**
+ * Represents a command to add a ToDo task.
+ */
 public class ToDoCommand extends Command {
+    /**
+     * Constructs a ToDoCommand with the full command string.
+     *
+     * @param fullCommand The full user input command string.
+     */
     public ToDoCommand(String fullCommand) {
         super(fullCommand);
     }
 
+    /**
+     * Executes the ToDo command, adding a new ToDo task to the list.
+     *
+     * @param tasks The list of tasks.
+     * @param ui The user interface handler.
+     * @param save The save handler for persisting tasks.
+     * @throws TinkertonException If the command format is invalid.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Save save) throws TinkertonException {
         String fullCommand = super.getFull();
@@ -30,6 +46,11 @@ public class ToDoCommand extends Command {
         save.save(tasks);
     }
 
+    /**
+     * Indicates whether this command should exit the application.
+     *
+     * @return false, as adding a ToDo does not exit the application.
+     */
     @Override
     public boolean isExit() {
         return false;
