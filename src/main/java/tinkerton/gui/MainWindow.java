056085d4b213df4dc64df600b901d50be1b4df9a
ctrf03 <clintontayrf.ct@gmail.com>
2025-09-02 20:44:14 +0800

    Fix bug on image name

--- src/main/java/tinkerton/gui/MainWindow.java
+++ src/main/java/tinkerton/gui/MainWindow.java
@@ -26,7 +26,8 @@ public class MainWindow extends AnchorPane {
     private Tinkerton tinkerton;
 
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image dukeImage =
+            new Image(this.getClass().getResourceAsStream("/images/DaTinkerton.png"));
 
     @FXML
     public void initialize() {
